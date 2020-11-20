6865a7146da8e572044d6a7270f1ae9bcd2fcc2a
hajnii <ksb12213@naver.com>
2020-11-20 10:17:36 +0900

    초기구성

--- app/src/main/java/com/example/hive/admin/AdminJoin.java
+++ app/src/main/java/com/example/hive/admin/AdminJoin.java
@@ -2,10 +2,12 @@ package com.example.hive.admin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -18,6 +20,7 @@ public class AdminJoin extends AppCompatActivity {
 
     EditText adminJoinEmail;
     Button btnCertified;
+    Button btnAdminJoin;
 
     String TAG = "AAA";
 
@@ -31,8 +34,21 @@ public class AdminJoin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_join);
         emailCategory = findViewById(R.id.emailCategory);
+        btnAdminJoin = findViewById(R.id.btnAdminJoin);
+        adminJoinEmail = findViewById(R.id.adminJoinEmail);
+
         arrayAdapter = ArrayAdapter.createFromResource(this, R.array.spinner_email, android.R.layout.simple_spinner_dropdown_item);
         emailCategory.setAdapter(arrayAdapter);
+
+
+        // 이메일Text 영문 키보드로 키우기
+        adminJoinEmail.setPrivateImeOptions("defaultInputmode=english;");
+
+
+
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(adminJoinEmail, 0); // 키보드 보이기
+//        imm.hideSoftInputFromWindow(adminJoinEmail.getWindowToken(),0); 키보드 숨기기 ex)검색버튼 눌릴 시 사용
 
 //        AdapterView.OnItemSelectedListener()
         emailCategory.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -41,17 +57,13 @@ public class AdminJoin extends AppCompatActivity {
                 switch (categoryType) {
                     case 0:
                         Log.i(TAG, Integer.toString(categoryType));
-                        category = null;
+                        category = "naver.com";
                         break;
                     case 1:
                         Log.i(TAG, Integer.toString(categoryType));
-                        category = "naver.com";
-                        break;
-                    case 2:
-                        Log.i(TAG, Integer.toString(categoryType));
                         category = "gmail.com";
                         break;
-                    case 3:
+                    case 2:
                         Log.i(TAG, Integer.toString(categoryType));
                         category = "hanmail.com";
                         break;
@@ -64,13 +76,27 @@ public class AdminJoin extends AppCompatActivity {
             }
         });
 
-        btnCertified.setOnClickListener(new View.OnClickListener() {
+//        btnCertified.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
+
+
+
+        btnAdminJoin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+
+                Intent i = new Intent(AdminJoin.this,AdminMain.class);
+
+                i.putExtra("TextOut", adminJoinEmail.getText().toString());
+                i.putExtra("catagory", category);
+                startActivity(i);
             }
         });
-
 
 
 
--- app/src/main/java/com/example/hive/admin/AdminMain.java
+++ app/src/main/java/com/example/hive/admin/AdminMain.java
@@ -1,16 +1,27 @@
 package com.example.hive.admin;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.hive.R;
 
 public class AdminMain extends AppCompatActivity {
 
+    TextView txtMain;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_main);
+        txtMain = findViewById(R.id.txtMain);
+
+        Intent intent = getIntent();
+        txtMain.setText(intent.getExtras().getString("TextOut") + "@" + intent.getExtras().getString("catagory"));
+
     }
+
 }
